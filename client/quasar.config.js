0e8389d2fe6d2cf4b4931e3a14790e03c6a6df17
Edi Aucapina <EDia@nycstudents.net>
2024-01-30 16:11:08 -0500

    Updated PROD URL in quasar.config.js

--- client/quasar.config.js
+++ client/quasar.config.js
@@ -62,7 +62,7 @@ module.exports = configure(function ( ctx ) {
 
       vueRouterMode: 'history', // available values: 'hash', 'history'
       env: {
-        SERVER_URL: ctx.dev ? 'http://localhost:3000' : 'http://<YOUR_PROD_IP>',
+        SERVER_URL: ctx.dev ? 'http://localhost:3000' : 'http://54.204.211.211',
         CREATOR_NAME: 'Edi Aucapina',
         CREATOR_EMAIL: 'Edaucapina@gmail.com',
         LINKEDIN: 'https://y7gy`.linkedin.com/in/ediaucapina/'
